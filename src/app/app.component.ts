0c825e9ae8252225ee71ea2e13ee254b5969e4f3
angel <angelcomu04@gmail.com>
2024-10-24 19:41:03 +0200

    material metido y login empezao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PerfilUsuarioComponent } from "./perfil/perfil-usuario/perfil-usuario.component";
+import { LoginComponent } from "./auth/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PerfilUsuarioComponent],
+  imports: [RouterOutlet, PerfilUsuarioComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
